4516bb92d2a372b9811b5b8ac841323bfc95f617
Anupama Tuli <anutuli@microsoft.com>
2019-10-03 15:44:25 -0700

    update index.js to test the env var

--- actions/AddComment/index.js
+++ actions/AddComment/index.js
@@ -9,31 +9,6 @@ const octokit = new Octokit({
     auth: '${token}'
 });
 
-
-/* try {
-    const owner = github.context.repo core.getInput('repo').split('/')[0];
-    console.log(`Hello ${owner}!`);
-    const repo = core.getInput('repo').split('/')[1];
-    console.log(`Hello ${repo}!`);
-    const pull_number = core.getInput('pull_number');
-    console.log(`Hello ${pull_number}!`);
-    const body = core.getInput('body');
-    console.log(`Hello ${body}!`);
-    const commit_id = core.getInput('commit_id');
-    console.log(`Hello ${commit_id}!`);
-    const path = core.getInput('path');
-    console.log(`Hello ${path}!`);
-    const position = core.getInput('position');
-    console.log(`Hello ${position}!`);
-    const token = core.getInput('token');
-    console.log(`Hello ${token}`);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
-} catch (error) {
-    core.setFailed(error.message);
-} */
-
 try {
     const repo = process.env.GITHUB_REPOSITORY.toString();
     var repoNameWithOwnerArray = repo.split("/", 2); 
@@ -53,7 +28,7 @@ try {
     console.log(`Hello path ${path}!`);
     const position = core.getInput('position');
     console.log(`Hello position ${position}!`);
-    const token = process.env.GITHUB_TOKEN.toString();
+    const token = process.env.TOKEN.toString();
     console.log(`Hello token ${token}`);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
