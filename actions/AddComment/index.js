c54d673ccb068a253f118735e2f0d40368692f01
Anupama Tuli <anutuli@microsoft.com>
2019-10-03 14:33:21 -0700

    update index.js to test the env var

--- actions/AddComment/index.js
+++ actions/AddComment/index.js
@@ -35,36 +35,37 @@ const octokit = new Octokit({
 } */
 
 try {
-    const repo = github.GitHub.GITHUB_REPOSITORY;
-    const owner = repo.owner.toString();
-    const actualRepo = repo.repo.toString();
+    const repo = process.env.GITHUB_REPOSITORY.toString();
+    var repoNameWithOwnerArray = repo.split("/", 2); 
+    const owner = repoNameWithOwnerArray[0];
+    const actualRepo = repoNameWithOwnerArray[1];
     console.log(`Hello owner ${owner}!`);
-    const private_spc = process.env['GITHUB_PRIVATE_SPACE'];
+    const private_spc = process.env.GITHUB_PRIVATE_SPACE;
     console.log(`Hello  private ${private_spc}!`);
     console.log(`Hello  repo ${actualRepo}!`);
     const pull_number = core.getInput('pull_number');
     console.log(`Hello pull_number ${pull_number}!`);
     const body  = JSON.stringify(core.getInput('body'), undefined, 2);
     console.log(`Hello body ${body}!`);
-    const commit_id = github.GitHub.GITHUB_SHA;
+    const commit_id = process.env.GITHUB_SHA;
     console.log(`Hello commit_id ${commit_id}!`);
     const path = core.getInput('path');
     console.log(`Hello path ${path}!`);
     const position = core.getInput('position');
     console.log(`Hello position ${position}!`);
-    const token = process.env['GITHUB_TOKEN'];
+    const token = process.env.GITHUB_TOKEN.toString();
     console.log(`Hello token ${token}`);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
-    const headref = github.GitHub.GITHUB_HEAD_REF;
-    const baseref = github.Github.GITHUB_BASE_REF;
+    const headref = process.env.GITHUB_HEAD_REF.toString();
+    const baseref = process.env.GITHUB_BASE_REF.toString();
     console.log(`hello headref ${headref}`);
     console.log(`hello baseref ${baseref}`);
     
-    const host = process.env['GITHUB_HOST_SUFFIX'];
+    const host = process.env.GITHUB_HOST_SUFFIX.toString();
     
-    const parent = process.env['GITHUB_PARENT_SPACE'];
+    const parent = process.env.GITHUB_PARENT_SPACE.toString;
     if("dev"==parent)
     {
         console.log(`###############yes! this is the parent space secret`);
