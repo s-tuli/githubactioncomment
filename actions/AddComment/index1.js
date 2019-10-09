125b415e37356523cdc9e676c072f35bf13fb272
Anupama Tuli <anutuli@microsoft.com>
2019-10-09 15:06:06 -0700

    update\

--- actions/AddComment/index1.js
+++ actions/AddComment/index1.js
@@ -35,8 +35,7 @@ function findPullRequestSubjectIdAndAddCommentToThatPullRequest(owner, repo, tok
     let findPullRequestIdQuery = `query FindPullRequestID ($owner: String!, $repo: String!){
   repository(owner:$owner, name:$repo) {
     pullRequest(number:73) {
-      id
-    
+      id    
   }
 }`;
 
